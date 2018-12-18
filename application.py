8c5145165b08d7878416b3776e071b8d21796bb0
Jingyi Mo <jingyim62015@gmail.com>
2018-12-18 15:02:36 -0500

    change host

--- application.py
+++ application.py
@@ -389,4 +389,4 @@ def deleteAttraction(city_id, attraction_id):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port="80")
+    app.run(port="80")
