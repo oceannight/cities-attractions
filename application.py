dcb2dae451cdc554342b6c0ca70cf59cdaafe400
oceannight <43579227+oceannight@users.noreply.github.com>
2018-12-19 11:21:43 -0500

    update port to 80

--- application.py
+++ application.py
@@ -387,5 +387,5 @@ def deleteAttraction(city_id, attraction_id):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=80)
 
